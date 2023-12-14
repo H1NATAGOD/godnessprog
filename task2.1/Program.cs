895e727385276f24b5c92e1a107e4d4a8535eb40
H1NATAGOD <dimonadardas@mail.ru>
2023-12-14 22:19:40 +0700

    task1.1-3.1

---
+++ task2.1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[100];
+            Console.WriteLine("Get number: ");
+            int numb = int.Parse(Console.ReadLine());
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = numb;
+                numb = numb - 3;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine($"Number {i} = {nums[i]}");
+            }
+
+        }
+    }
+}
