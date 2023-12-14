2e3cb9fe5937d8b194ce2a504a74722c23f81325
H1NATAGOD <dimonadardas@mail.ru>
2023-12-14 22:16:42 +0700

    second commid

--- task1.4/Program.cs
+++ task1.4/Program.cs
@@ -1,12 +1,26 @@
-﻿namespace ConsoleApp1;
+﻿using System;
 
-public class Program
+namespace ConsoleApp1
 {
-
-    static void Main(string[] args)
+    class Program
     {
-        int[] nums = new int[];
-        int n1 = int.Parse(Console.ReadLine());
-        int n2 = int.Parse(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Set a limit for the number of numbers ");
+            int numb = int.Parse(Console.ReadLine());
+            int[] nums = new int[numb];
+            Random rnd1 = new Random();
+            Console.WriteLine("Set limits for random numbers, the first number is less than the second");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numb; i++)
+            {
+                nums[i] = rnd1.Next(n1, n2);
+            }
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("{0} ", nums[i]);
+            }
+        }
     }
-}    
+}  
