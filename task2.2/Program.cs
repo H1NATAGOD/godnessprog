fa34f099ea5787588c4fd364a84191a9d3c124ee
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-16 09:45:22 +0700

    Add files via upload

---
+++ task2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1;
+
+public class Program
+{
+    static void Main(string[] args)
+    { 
+        Console.Write("Set the number of numbers in the array: ");
+        int numb = int.Parse(Console.ReadLine());
+        int[] nums = new int[numb];
+        for (int i = 1; i <= numb; i++)
+        {
+            if (i % 2 != 0)
+            {
+                nums[i] = i ;
+               
+            }
+            Console.WriteLine($"нечётное число массива {nums[i]}");
+        }
+    }
+}   
---
+++ task2.3/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ConsoleApp1;
+
+ public class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите размерность матрицы: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[n, n];
+
+        // Заполнение первой строки и первого столбца значением 1
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i, 0] = 1;
+            matrix[0, i] = 1;
+        }
+
+        // Заполнение остальных элементов матрицы
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                matrix[i, j] = matrix[i - 1, j] + matrix[i, j - 1];
+            }
+        }
+
+        // Вывод матрицы на экран
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+} 
