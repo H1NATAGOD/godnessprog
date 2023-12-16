cb45a276d8aec8cf64d3e9ad0698db476b35e231
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-16 11:10:20 +0700

    Ryo

---
+++ task3.2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1;
+
+public class Program
+{
+    static void Main(string args[])
+    {
+        List<int> nums = new List<int>();
+        using (StreamReader reader = new StreamReader("input.txt"))
+        {
+            string numsString = reader.ReadLine() ?? string.Empty; 
+            nums = numsString.Split(' ').Select(int.Parse).ToList();
+        }
+               
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+                nums.RemoveAt(i);
+                i--
+            }
+        }
+
+        foreach (int num in nums)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
+
