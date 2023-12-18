9b1fdf9b723c9951842fbb57c10fa9e764caafcc
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-18 23:02:42 +0700

    Nijika

---
+++ task6.3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Get number: ");
+            int numb = int.Parse(Console.ReadLine());
+            if (numb % 2 == 0 && numb % 10 == 0)
+            {
+                Console.WriteLine($"Number {numb} even and multiple of 10");
+            }
+            else if (numb % 2 == 0 && numb % 10 != 0)
+            {
+                Console.WriteLine($"Number {numb} even, but not a multiple of 10");
+            }
+            else
+            {
+                Console.WriteLine($"Number {numb} not even and not a multiple of 10");
+            }
+        }
+    }
+}
