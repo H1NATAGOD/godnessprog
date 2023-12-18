c09ddd9f68fdf1ef792493a76f0e0c8094e5e8d1
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-18 17:41:58 +0700

    Nijika

---
+++ task5.1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("numsTask1.txt"))
+            {
+                string line = reader.ReadLine();
+                string[] nums = line.Split(' ');
+                reader.Close();
+                int min = int.MaxValue;
+                int sum = 0;
+                int index = 0;
+
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    int num = int.Parse(nums[i]);
+                    if (num < min)
+                    {
+                        index = i; 
+                        min = num;
+                    }
+
+                    if (num > min) 
+                    {
+                        sum += num; 
+                    }
+                }
+                Console.WriteLine($"Sum {min} is {sum} ");
+            }
+        }
+    }
+}
+                
