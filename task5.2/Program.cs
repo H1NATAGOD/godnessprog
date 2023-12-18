7eab07cba26e6f1f2022b0039200bb637ce5d5f1
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-18 18:08:43 +0700

    Nijika

---
+++ task5.2/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>();
+
+            using (StreamReader reader = new StreamReader("numsTask2.txt"))
+            {
+                string line = reader.ReadLine();
+                string[] numsStr = line.Split(';');
+                
+                for (int i = 0; i < numsStr.Length; i++)
+                {
+                    nums.Add(int.Parse(numsStr[i]));
+                }
+
+                nums.Sort();
+            }
+
+            string sortedNumsStr = string.Join(";", nums);
+
+            using (StreamWriter writer = new StreamWriter("numsTask2.txt"))
+            {
+                writer.Write(sortedNumsStr);
+            }
+
+            Console.WriteLine("Числа были успешно отсортированы и записаны обратно в файл.");
+        }
+    }
+}
