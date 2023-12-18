b4894521f45feaf214963d2d630d35f9530eabb9
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-18 10:49:19 +0700

    Add files via upload

--- task3.2/Program.cs
+++ task3.2/Program.cs
@@ -2,27 +2,35 @@
 
 public class Program
 {
-    static void Main(string args[])
+    static void Main()
     {
         List<int> nums = new List<int>();
-        using (StreamReader reader = new StreamReader("input.txt"))
+        using (StreamReader reader = new StreamReader("nums.txt"))
         {
             string numsString = reader.ReadLine() ?? string.Empty; 
             nums = numsString.Split(' ').Select(int.Parse).ToList();
         }
                
-        for (int i = 0; i < nums.Length; i++)
+        for (int i = 0; i < nums.Count; i++)
         {
             if (nums[i] % 2 == 0)
             {
                 nums.RemoveAt(i);
-                i--
+                i--;
             }
         }
 
         foreach (int num in nums)
         {
             Console.WriteLine(num);
+        }
+
+        using (StreamWriter writer = new StreamWriter("nums2.txt"))
+        {
+            foreach (int num in nums)
+            {
+                writer.WriteLine(num);
+            }
         }
     }
 }
