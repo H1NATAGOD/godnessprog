176927fd4668dfa26407e457d3af6f4067d041a2
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-18 20:12:51 +0700

    Nijika

---
+++ task6.1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("numsTask1.txt"))
+            {
+                string line = reader.ReadLine();
+                string[] words = line.Split(' ');
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    string word = words[i];
+                    if (word.Length % 2 != 0)
+                    {
+                        Console.WriteLine(word);
+                    }
+                }
+            }
+        }
+    }
+}
