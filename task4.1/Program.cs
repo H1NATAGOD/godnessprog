a93aa8bd5429ee74a0dc6563acb27162befbbc3a
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-18 10:50:16 +0700

    Add files via upload

---
+++ task4.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+using System.Runtime.InteropServices.JavaScript;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Specify a positive integer: ");
+            BigInteger n = BigInteger.Parse(Console.ReadLine());
+            
+            BigInteger sum = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                if ( i % 3 == 0)
+                {
+                    sum *= i;
+                }
+                Console.WriteLine(sum );
+            }
+            Console.WriteLine($"The product of number is equal to {sum}");
+        }
+    }
+}    
