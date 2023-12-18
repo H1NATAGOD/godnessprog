284c2cd2af8cf675cf82e7ef8b07b226253d26a4
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-18 10:47:14 +0700

    Add files via upload

---
+++ task4.3/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("numsTask3.txt"))
+            {
+                string line = reader.ReadLine();
+                string[] nums = line.Split(',');
+                reader.Close();
+
+                int max = int.Parse(nums[0]);
+                int min = int.Parse(nums[0]);
+                for (int i = 0; i < nums.Length; ++i)
+                {
+                    int value = int.Parse(nums[i]);
+                    
+                    if (value == 0) 
+                    {
+                        break;
+                    }
+                    
+                    if (value < min)
+                    {
+                        min = value;
+                    }
+
+
+                    if (value > max)
+                    {
+                        max = value;
+                    }
+                }
+
+
+                int result = max - min;
+                
+                Console.WriteLine($"Resalt is {result}");
+            }
+        }
+    }
+}
