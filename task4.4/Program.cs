c298c4e5e1f62c96a42c40f0fd42b0ef1392c319
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-18 11:02:01 +0700

    bocchi)

---
+++ task4.4/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("numsTask4.txt"))
+            {
+                string line = reader.ReadLine();
+                string[] nums = line.Split(' ');
+                reader.Close();
+
+                int count = 0;
+
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    if (nums[i] == nums[i + 1])
+                    {
+                        count++;
+                    }
+                    
+                }
+                Console.WriteLine($"umber of identical numbers is {count}");
+            }
+        }
+    }
+}
