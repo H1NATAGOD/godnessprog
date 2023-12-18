2b57467e87b2743621ca82b3f32da26903b928b2
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-18 10:48:17 +0700

    Add files via upload

---
+++ task4.2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("numsTask2.txt"))
+            {
+                string line = reader.ReadLine();
+                string[] nums = line.Split(';');
+                reader.Close();
+
+                double sum = 0;
+                for (int i = 0; i < nums.Length; ++i)
+                {
+                    double value = double.Parse(nums[i]);
+                    if (value == 0)
+                    {
+                        break;
+                    }
+
+                    if (value > 0)
+                    {
+                        sum = sum + value;
+                    }
+                }
+                Console.WriteLine($"sum is {sum}");
+            }
+        }
+    }
+}
