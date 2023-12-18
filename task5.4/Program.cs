496d47d23837f63061fb672529dea11eb14bad5d
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-18 18:32:35 +0700

    Nijika

---
+++ task5.4/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("numsTask4.txt"))
+            {
+                string line = reader.ReadLine();
+                string[] nums = line.Split(' ');
+                reader.Close();
+                int max = int.MinValue;
+                int sum = 0;
+                int index = 0;
+
+                for (int i = 0; i < nums.Length - 1; i++)
+                {
+                    int num = int.Parse(nums[i]);
+                    if (num > max)
+                    {
+                        index = i;
+                        max = num;
+                    }
+                    
+                }
+
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int num2 = int.Parse(nums[i]);
+                    if (max - 1 == num2)
+                    {
+                        sum += num2;
+                    }
+                }
+              
+
+                Console.WriteLine($"Maximal number: {max}");
+                Console.WriteLine($"Sum of numbers differing from the maximum by 1: {sum}");
+                
+            }
+        }
+    }
+}
