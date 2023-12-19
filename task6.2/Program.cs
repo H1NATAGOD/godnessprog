a93123c04dcc5b355a1d3da69e4e5f49a5eff6f9
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-19 09:59:46 +0700

    Bocchi

---
+++ task6.2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = File.ReadAllLines("../../../numsTask2.txt");
+            string fString = string.Join(" ", words);
+            Console.WriteLine(fString); 
+        }
+    }
+}
