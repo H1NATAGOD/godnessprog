76c676ffa600b714142a0cd5bda5e8e28b9dc3ec
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-19 13:10:29 +0700

    Bocchi

---
+++ task6.4/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Enter a positive number, if it is negative, the program will end");
+        int sum = 0;
+        List<int> nums = new List<int>();
+            
+        nums.Add(int.Parse(Console.ReadLine()));
+        int numb;
+        int posnumb;
+        do
+        {
+            numb = int.Parse(Console.ReadLine());
+            nums.Add(numb);
+        }   
+        while (numb > 0 );
+
+        for (int i = 0; i < nums.Count; i++)
+        {
+            posnumb = nums[i];
+            sum += posnumb;
+        }
+        Console.WriteLine($"The sum of positive numbers is: {sum}");
+
+        }
+    }
+}
