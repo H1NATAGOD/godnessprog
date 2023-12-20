a5b138893a775594d4ea61803f39e1d251fe0fb6
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-20 09:59:10 +0700

    Bocchi

--- task3.1/Program.cs
+++ task3.1/Program.cs
@@ -1,6 +1,56 @@
-﻿int[] winnums = int [10] ;
-using StreamReader reader = new StreamReader(path, false);
-reader.BaseStream.Position = 0;
-for()
-reader.ReadLine();
-for()
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lottery
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] lines = File.ReadAllLines("../../../input.txt");
+            string[] selectedNumbers = lines[0].Split(' ');
+            int n = int.Parse(lines[1]);
+            string[] tickets = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                tickets[i] = lines[i + 2];
+            }
+
+            string[] results = CheckTickets(selectedNumbers, tickets);
+
+            File.WriteAllLines("../../../output.txt", results);
+        }
+
+        static string[] CheckTickets(string[] selectedNumbers, string[] tickets)
+        {
+            string[] results = new string[tickets.Length];
+
+            for (int i = 0; i < tickets.Length; i++)
+            {
+                string[] ticketNumbers = tickets[i].Split(' ');
+                int count = 0;
+
+                for(int j = 0; j < selectedNumbers.Length; j++)
+                {
+                    if (Array.IndexOf(ticketNumbers, selectedNumbers[j]) != -1)
+                    {
+                        count++;
+                    }
+                }
+
+                if (count >= 3)
+                {
+                    results[i] = "Lucky";
+                }
+                else
+                {
+                    results[i] = "Unlucky";
+                }
+            }
+
+            return results;
+        }
+    }
+}
