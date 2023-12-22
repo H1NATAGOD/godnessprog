d862b1681741a85847de95779bd72d735bf0f595
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-22 09:29:28 +0700

    Bocchi

---
+++ task6.5/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int[,] nums = new int[n, m];
+            int[] count = new int[n];
+            int sum = 0;
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    nums[i, j] = rnd.Next(0, 2);
+                    Console.Write($"{nums[i, j]} \t");
+                    sum += nums[i, j];
+                }
+
+                if (sum % 2 != 0)
+                {
+                    count[i] = 1;
+                }
+
+                sum = 0;
+
+                Console.WriteLine(count[i]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
