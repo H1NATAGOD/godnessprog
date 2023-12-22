0d321108e52ac92b5d288347286fac1890fdee20
H1NATAGOD <153798651+H1NATAGOD@users.noreply.github.com>
2023-12-22 09:31:50 +0700

    bocchi

---
+++ task5.5/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "../../../numsTask5.txt";
+            string data = File.ReadAllText(path);
+            int[] nums = data.Split(' ').Select(int.Parse).ToArray();
+
+            int max = nums[0];
+            int maxIndex = 0;
+            int min = nums[0];
+            int minIndex = 0;
+            int srsum = 0;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > max)
+                {
+                    max = nums[i];
+                    maxIndex = i;
+                }
+                else if (nums[i] < min)
+                {
+                    min = nums[i];
+                    minIndex = i;
+                }
+            }
+
+            if (minIndex > maxIndex)
+            {
+                (maxIndex, minIndex) = (minIndex, maxIndex);
+            }
+
+            for (int i = minIndex + 1; i < maxIndex; i++)
+            {
+                srsum += nums[i];
+                Console.WriteLine(nums[i]);
+            }
+
+           int srproiz = srsum / (maxIndex - minIndex - 1);
+
+            Console.WriteLine($"srproiz is {srproiz}");
+        }
+    }
+}
